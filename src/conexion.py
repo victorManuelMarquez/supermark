e9cf4f268b44367f44d9cad90b5c2e68064204af
victor <victormmla3lm@gmail.com>
2022-11-21 12:33:28 -0300

    Update Checkpoint #3

---
+++ src/conexion.py
@@ -0,0 +1,24 @@
+import sqlite3 as sql
+
+class Conexion():
+
+    def __init__(self, bd):
+        self.__conexion = sql.connect(bd, detect_types=sql.PARSE_DECLTYPES | sql.PARSE_COLNAMES)
+        self.__cursor = self.__conexion.cursor()
+
+    def ejecutar(self, consulta):
+        self.__cursor.execute(consulta)
+        if 'INSERT' in consulta or 'UPDATE' in consulta or 'DELETE' in consulta:
+            self.__conexion.commit()
+    
+    def datos(self):
+        return self.__cursor.fetchall()
+
+    def columnas(self):
+        return [nombre[0] for nombre in self.__cursor.description]
+
+    def deshacer(self):
+        self.__conexion.rollback()
+    
+    def cerrar(self):
+        self.__conexion.close()
--- src/supermark-main-nogui.py
+++ src/supermark-main-nogui.py
@@ -1,33 +1,10 @@
 import os
-import sqlite3 as sql
 import pandas
+import conexion as bd
 
 # devuelve la ruta absoluta a la base de datos (necesario para operar localmente)
 def ruta_absoluta(bd_path):
     return os.path.join(os.path.dirname(__file__), bd_path)
-
-class Conexion():
-
-    def __init__(self, bd):
-        self.__conexion = sql.connect(bd, detect_types=sql.PARSE_DECLTYPES | sql.PARSE_COLNAMES)
-        self.__cursor = self.__conexion.cursor()
-
-    def ejecutar(self, consulta):
-        self.__cursor.execute(consulta)
-        if 'INSERT' in consulta or 'UPDATE' in consulta or 'DELETE' in consulta:
-            self.__conexion.commit()
-    
-    def datos(self):
-        return self.__cursor.fetchall()
-
-    def columnas(self):
-        return [nombre[0] for nombre in self.__cursor.description]
-
-    def deshacer(self):
-        self.__conexion.rollback()
-    
-    def cerrar(self):
-        self.__conexion.close()
 
 def mostrar_tabla(conexion, tabla):
     print("Tabla:", tabla)
@@ -35,9 +12,9 @@ def mostrar_tabla(conexion, tabla):
     print(pandas.DataFrame.from_records(data=conexion.datos(), columns=conexion.columnas()))
 
 #********** TEST **********#
-conexion = Conexion(ruta_absoluta('supermark-data.db'))
+conexion = bd.Conexion(ruta_absoluta('supermark-data.db'))
 mostrar_tabla(conexion, 'personas')
 mostrar_tabla(conexion, 'categorias')
-mostrar_tabla(conexion, 'articulos_detalles')
+mostrar_tabla(conexion, 'productos')
 mostrar_tabla(conexion, 'ventas_detalles')
 conexion.cerrar()
