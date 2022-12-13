30d5db6a5f9446bbb71b94de65a422feced4296b
victor <victormmla3lm@gmail.com>
2022-12-13 18:50:06 -0300

    useless documentation removed

--- src/supermark/app.py
+++ src/supermark/app.py
@@ -3,29 +3,8 @@ import componente as cmp
 
 
 class App():
-    """
-    Una clase para contener la aplicación gráfica principal.
-
-    ...
-    
-    métodos
-    -------
-    root()
-        Devuelve la aplicación gráfica principal.
-    preferredSize(w: int, h: int)
-        Establece el tamaño de la ventana y la centra.
-    iniciar()
-        Esta función inicia la interfaz gráfica.
-    """
 
     def __init__(self, titulo="Super Mark"):
-        """
-        parámetros
-        ----------
-        titulo : str, opcional
-            título para la ventana
-        """
-
         self.__root = Tk()
         self._defaultFont = font.nametofont('TkDefaultFont')
         self._defaultFont.configure(family="Segoe Script", size=11)
@@ -44,36 +23,14 @@ class App():
         return self.__root
 
 
-    root.__doc__ = "Devuelve el componente gui."
-
-
     def preferredSize(self, w: int, h: int):
-        """
-        parámetros
-        ----------
-        w : int
-            ancho determinado.
-        h : int
-            alto determinado.
-        """
-
         sw = self.__root.winfo_screenwidth()
         sh = self.__root.winfo_screenheight()
         self.__root.geometry('%dx%d+%d+%d' % (w, h, (sw-w)/2, (sh-h)/2))
 
 
-    preferredSize.__doc__ = "Establece el tamaño de la ventana y la centra."
-
-
     def iniciar(self):
-        """
-        Invoca a la función mainloop()
-        """
-        
         self.root.mainloop()
-
-
-    iniciar.__doc__ = "Esta función inicia la interfaz gráfica."
 
 
 app = App()
